f3b2dec3535c41efbfa8105fe5021c38327f6841
Roman <romak8162@gmail.com>
2025-08-07 19:06:57 +0300

    make build

---
+++ index.68cdd24c.js
@@ -0,0 +1,2 @@
+function n(){var n=document.body;if("#aside"===window.location.hash){var d=window.innerWidth-document.documentElement.clientWidth;n.style.paddingRight=d+"px",n.classList.add("no-scroll")}else n.style.paddingRight="",n.classList.remove("no-scroll")}window.addEventListener("hashchange",n),n();
+//# sourceMappingURL=index.68cdd24c.js.map
